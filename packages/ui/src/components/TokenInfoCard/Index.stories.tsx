20ec261122770988b03915a46a3754a9fdde07d7
luckmer <woszczo04@gmail.com>
2024-10-14 18:39:07 +0200

    token info card

---
+++ packages/ui/src/components/TokenInfoCard/Index.stories.tsx
@@ -0,0 +1,24 @@
+import { type Meta, type StoryObj } from "storybook-solidjs";
+import TokenInfoCard from "./Index";
+
+const meta: Meta<typeof TokenInfoCard> = {
+  component: TokenInfoCard,
+  title: "Components/TokenInfoCard",
+};
+export default meta;
+type Story = StoryObj<typeof TokenInfoCard>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <div class="m-[20px] flex flex-col gap-[1px] w-full">
+        <TokenInfoCard label="Label" description="Description" isFirst={true} />
+        <TokenInfoCard
+          label="Label"
+          description="Description"
+          isFirst={false}
+        />
+      </div>
+    );
+  },
+};
